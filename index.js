dc1dc7a398be5541f84d328f9c8b3035264f8958
Ahmed <ahmad.rizgar.rashid@gmail.com>
2021-01-31 14:05:46 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("hello world")
+
+document.getElementsByClassName("button1").addEventListener("click", function() {
+    alert("Hello World!");
+  });
+// console.log(x)
+// x[0].addEventListener("click", function() {
+//   alert("Hello World!");
+// });
+// document.getElementById("button1")
+// document.getElementById("button1").addEventListener("click", ()=> console.log("Hello World!"))
+var x = document.getElementsByClassName("awesomeDiv").
+console.log(x)
